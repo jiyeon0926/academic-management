fb7af426cbcb5ac43e9de58abcab733576f49f4d
jiyeon0926 <yeoni9094@naver.com>
2025-03-23 00:19:33 +0900

    fix: DepartmentResDto 필드 수정
    - 생성일자, 수정일자 추가

--- src/main/kotlin/com/kotlin/academic/domain/department/dto/DepartmentResDto.kt
+++ src/main/kotlin/com/kotlin/academic/domain/department/dto/DepartmentResDto.kt
@@ -1,15 +1,20 @@
 package com.kotlin.academic.domain.department.dto
 
 import com.kotlin.academic.domain.department.entity.Department
+import java.time.LocalDateTime
 
 data class DepartmentResDto(
     val id: Long?,
     val code: String,
-    val name: String) {
+    val name: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime) {
 
     constructor(department: Department) : this(
         id = department.id,
         code = department.code,
-        name = department.name
+        name = department.name,
+        createdAt = department.createdAt,
+        updatedAt = department.updatedAt
     )
 }
