df96e3991cd69a02ad47d8723551c98baf95303e
jiyeon0926 <yeoni9094@naver.com>
2025-03-23 18:31:18 +0900

    feat: REQUIRED_FIELD_MISSING error code 추가
    - 400 상태코드
    - 필수값이 없을 때

--- src/main/kotlin/com/kotlin/academic/global/error/ErrorCode.kt
+++ src/main/kotlin/com/kotlin/academic/global/error/ErrorCode.kt
@@ -6,6 +6,9 @@ enum class ErrorCode(
     val status: HttpStatus,
     val message: String) {
 
+    // 400
+    REQUIRED_FIELD_MISSING(HttpStatus.BAD_REQUEST, "필수값을 입력해주세요."),
+
     // 404 NOT_FOUND
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     DEPARTMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "학과를 찾을 수 없습니다."),
